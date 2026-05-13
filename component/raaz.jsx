a58e69601e855dddcfa099115976a46d58d7da1c
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-05-13 19:48:46 +0530

    bug fix and UI change

--- component/raaz.jsx
+++ component/raaz.jsx
@@ -1,4 +1,5 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { CiSearch } from "react-icons/ci";
 
 import './raaz.css'
 
@@ -6,15 +7,13 @@ const App = () => {
 
     const [wpid, setWpid] = useState("")
     const [userdata, setUserdata] = useState()
+    const [typingText, setTypingText] = useState("")
 
     const raaz = async (e) => {
-
-        // const wep = Number(wp)
 
         e.preventDefault()
 
         try {
-
 
             const urldata = await fetch(
                 `https://server-backend-1-1nf0.onrender.com/?wpid=${wpid}`
@@ -24,31 +23,79 @@ const App = () => {
 
             setUserdata(raazdata)
 
-            if(raazdata !== null){
-
-            localStorage.setItem('info',JSON.stringify(raazdata))
+            if (raazdata !== null) {
+                localStorage.setItem('info', JSON.stringify(raazdata))
             }
-        }
 
+        }
 
         catch (err) {
             console.log(err.message)
         }
 
-
     }
 
-    useEffect(()=>{
+
+    // LocalStorage Data
+    useEffect(() => {
+
         const data = localStorage.getItem('info')
 
-        if(data){
+        if (data) {
 
-        const convt = JSON.parse(data)
+            const convt = JSON.parse(data)
 
-        setWpid(convt.weplayid)
+            setWpid(convt.weplayid)
+        }
+
+    }, [])
+
+
+
+    // Typing Animation
+    useEffect(() => {
+
+        let fullText = ""
+
+        if (userdata === null) {
+
+            fullText = "You Are Not My Friend"
 
         }
-    },[userdata])
+
+        else if (!userdata) {
+
+            fullText = "Please enter Wpid"
+
+        }
+
+        else {
+
+            fullText = `My Name Is ${userdata?.name}`
+
+        }
+
+        let index = 0
+
+        setTypingText("")
+
+        const interval = setInterval(() => {
+
+            setTypingText(fullText.slice(0, index + 1))
+
+            index++
+
+            if (index === fullText.length) {
+
+                clearInterval(interval)
+
+            }
+
+        }, 100)
+
+        return () => clearInterval(interval)
+
+    }, [userdata])
 
 
 
@@ -56,73 +103,86 @@ const App = () => {
 
         <>
 
-        <div className='maindiv'>
-            {!userdata
-                ? <h1>Please enter wpid</h1>
-                : <h1>My Name Is {userdata?.name}</h1>
-            }
+            <div className='maindiv'>
+
+                <h1>{typingText}</h1>
+
+            </div>
+
+
+            <div className='fromdiv'>
+
+                <form onSubmit={raaz}>
+
+                    <input
+                        type='number'
+                        placeholder='input your wpid'
+                        required
+                        value={wpid}
+                        onChange={(e) => setWpid(e.target.value)}
+                    />
+
+                    <div className='dtndiv'>
+
+                        <button type='submit' className='btn'>
+                            Search <CiSearch />
+                        </button>
+
+                    </div>
+
+                </form>
 
             </div>
 
 
 
-            <form onSubmit={raaz}>
+            {userdata ?
 
-                <input
-                    type='number'
-                    placeholder='input your wpid'
-                    required
-                    value={wpid}
-                    onChange={(e) => setWpid(e.target.value)}
-                />
+                <div className='infodiv'>
 
-                <div className='dtndiv'>
+                    <div className='info'>
+                        <p>My weplay id is {userdata?.weplayid}</p>
+                    </div>
 
-                <button type='submit' className='btn'>
-                    Please Enter
-                </button>
+                    <div className='info'>
+                        <p>My Charms is {userdata?.charm}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My Signature is {userdata?.signature}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My bff Number is {userdata?.bff}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My gifts is {userdata?.gift}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My moments is {userdata?.moments}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My star is {userdata?.star}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>Time is {userdata?.time}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>Date is {userdata?.date}</p>
+                    </div>
+
+                    <div className='info'>
+                        <p>My Family name is {userdata?.family}</p>
+                    </div>
 
                 </div>
 
-            </form>
-
-
-            {userdata===null? (<h1>You Are Not My Friend</h1>)
-
-               : userdata?
-               ( <div className='infodiv'>
-                    <div className='info'>
-                    <p>My weplay id is {userdata?.weplayid}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My Charms is {userdata?.charm}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My Signature is {userdata?.signature}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My bff Number is {userdata?.bff}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My gifts is {userdata?.gift}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My moments is {userdata?.moments}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My star is {userdata?.star}</p>
-                    </div>
-                    <div className='info'>
-                    <p>Time is {userdata?.time}</p>
-                    </div>
-                    <div className='info'>
-                    <p>Date is {userdata?.date}</p>
-                    </div>
-                    <div className='info'>
-                    <p>My Family name is {userdata?.family}</p>
-                    </div>
-                </div>
-            ):null}
+                : null}
 
         </>
 
