f672d7bff5c54a688853d9872fea308bb2de72fb
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-05-15 07:37:17 +0530

    Update nav.jsx

--- component/nav.jsx
+++ component/nav.jsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 import '../component/nav.css'
 
 
-const App =({navlist,logoText})=>{
+const App =({logoText})=>{
     const[name,setName]=useState(0)
 
     return(
         <>
         <div className='navbar'>
-            <div className='logo'><span>{logoText}</span></div>
+            <div className='logo'>
+<span>{logoText}</span>
+
+</div>
             <ul>
                  <li><a href='/'>Home</a></li>
                  <li><a href='/contact'>Footer</a></li>
