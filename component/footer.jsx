baab94703203f4c0ba7d7c6278ad6d1fbf6915c6
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-05-17 13:33:34 +0530

    Fix some UI

--- component/footer.jsx
+++ component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './footer.css'
 
 
-const App = ({userInfo}) => {
+const App = ({ userInfo }) => {
 
     console.log(userInfo.url_profile)
 
@@ -11,23 +11,15 @@ const App = ({userInfo}) => {
             <div className="footer">
                 <div className="left">
                     <div className="leftImg">
-                        {userInfo.url?
-                        <img src={userInfo.url} alt="norendra"></img>
-                        :<img src='/modi.png' alt="norendra"></img>}
+                        {userInfo.url ?
+                            <img src={userInfo.url} alt="norendra"></img>
+                            : <img src='/modi.png' alt="norendra"></img>}
                     </div>
-                   {userInfo? <h1>{userInfo?.name}</h1>:<h1>Norendra Modi</h1>}
-                    <p>Prime Minister of India</p>
+                    {userInfo ? <h1>{userInfo?.name}</h1> : <h1>Norendra Modi</h1>}
+                    <p>Harami {userInfo?.name}</p>
                 </div>
-                <div className="right">
-                    <div className="rightImg">
-                        <img src="mamata.png" alt="Mamta"></img>
-                    </div>
-                    <h1>Mamata Banerjee</h1>
-                    <p> EX- Chief Minister of West Bengal
-                    </p>
-                </div>
-
             </div>
+
         </div>
     )
 }
